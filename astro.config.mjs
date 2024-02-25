31f2eda6ba76ad268718975c2299defb3336ea91
SGolomzin <s.golomzin@gmail.com>
2024-02-26 01:35:50 +0300

    lesson 5.5 - RSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://main--frabjous-rabanadas-5b9c7d.netlify.app"
+});

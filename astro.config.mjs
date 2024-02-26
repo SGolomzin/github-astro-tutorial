dc9f82805390f4f0d134df1c609460b080ea8ff8
SGolomzin <s.golomzin@gmail.com>
2024-02-26 23:07:16 +0300

    Refactor post pages to the Content Collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://main--frabjous-rabanadas-5b9c7d.netlify.app"
+  site: "https://main--frabjous-rabanadas-5b9c7d.netlify.app",
+  integrations: [preact()]
 });
